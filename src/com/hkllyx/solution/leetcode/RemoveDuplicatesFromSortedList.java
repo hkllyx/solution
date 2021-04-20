64a9b13859450693fa78c73bf7a2be159c89fbf4
xiaoyong3 <xiaoyong3@midea.com>
2021-04-20 15:00:54 +0800

    misc fix

--- src/com/hkllyx/solution/leetcode/RemoveDuplicatesFromSortedList.java
+++ src/com/hkllyx/solution/leetcode/RemoveDuplicatesFromSortedList.java
@@ -7,12 +7,10 @@ import com.hkllyx.solution.util.Test;
 import com.hkllyx.solution.util.TestUtils;
 
 /**
- * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list
- *
  * @author hkllyx
  * @date 2021/03/26
  */
-@Solution(no = 83, difficulty = Difficulty.SIMPLE, url = "")
+@Solution(no = 83, difficulty = Difficulty.SIMPLE, url = "https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list")
 public class RemoveDuplicatesFromSortedList {
 
     public static void main(String[] args) {
